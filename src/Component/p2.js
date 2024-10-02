251559dcd40e3303b22d9ba091c842377d32e870
Sivakess <sivakumarsivak807@gmail.com>
2024-10-01 21:06:17 -0700

    Delete src/Component directory
    remove unwanted component

--- src/Component/p2.js
+++
@@ -1,9 +0,0 @@
-function  p2(props) {
-    value=props.value
-    return(
-        <>
-        if{value}
-        </>
-    )
-
-}
--- src/Component/p4.js
+++
@@ -1,8 +0,0 @@
-function  p4() {
-    return(
-        <>
-        <h1>false</h1>
-        </>
-    )
-
-}
